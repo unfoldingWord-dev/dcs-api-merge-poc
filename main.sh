e5e61b3aaca28f39ce3624c592f7ff010639b5e3
Richard Mahn <richmahn@gmail.com>
2020-11-04 21:03:53 +0000

    Adds prompts

--- main.sh
+++ main.sh
@@ -6,14 +6,16 @@ repo="dcs-poc-repo"
 branch="user-tc-create-1"
 
 #USER INPUT
-read -p "Enter the name of the repo to use in the dcs-poc org [${repo}]: " repo_input
+read -p "Enter the name of the repo to use in the `dcs-poc-org` org [${repo}]: " repo_input
 repo=${repo_input:-$repo}
 
 read -p "Enter the name of the user branch [${branch}]: " branch_input
 branch=${branch_input:-$branch}
 
+echo Org: $org
 echo Repo: $repo
 echo User branch: $branch
+echo DCS URL: https://git.door43.org/$org/$repo
 
 #### FILE CONTENT
 file1_name="file1.md"
@@ -25,6 +27,31 @@ file2_name="file2.tsv"
 file2_content=$(cat "$file2_name")
 user_modified_file2_content=$(cat "user_modified_${file2_name}")
 master_modified_file2_content=$(cat "master_modified_${file2_name}")
+
+#### INIT DCS (DELETE OLD REPO IF EXISTS)
+echo "
+=========
+DELETING REPO $repo IF EXISTS:
+
+"
+response=$(curl --silent --write-out '%{http_code}' --output /dev/null -X DELETE "$host/api/v1/repos/$org/$repo?access_token=$token" -H "accept: application/json")
+if [[ "$response" == "404" ]]; then
+  echo "DOESN'T ALREADY EXIST, SKIPPING."
+else
+  echo "EXISTS. DELETED. $response"
+fi
+
+# CREATE REPO
+echo "
+=========
+CREATING REPO $repo:
+
+"
+response=$(curl --silent -X POST "$host/api/v1/orgs/$org/repos?access_token=$token" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"auto_init\": true, \"default_branch\": \"master\", \"description\": \"$repo\", \"license\": \"CC-BY-SA-4.0.md\", \"name\": \"$repo\", \"private\": false, \"readme\": \"Default\"}")
+echo "$response"
+
+read -p "
+Press ENTER to continue"
 
 echo "
 =========
@@ -60,34 +87,6 @@ FILE 2 ENCODED:
 "
 echo "$file2_base64"
 
-#### INIT DCS (DELETE OLD REPO IF EXISTS)
-echo "
-=========
-DELETING REPO $repo IF EXISTS:
-
-"
-response=$(curl --silent --write-out '%{http_code}' --output /dev/null -X DELETE "$host/api/v1/repos/$org/$repo?access_token=$token" -H "accept: application/json")
-if [[ "$response" == "404" ]]; then
-  echo "DOESN'T ALREADY EXIST, SKIPPING."
-else
-  echo "EXISTS. DELETED. $response"
-fi
-
-echo "Press ENTER to continue"
-read
-
-# CREATE REPO
-echo "
-=========
-CREATING REPO $repo:
-
-"
-response=$(curl --silent -X POST "$host/api/v1/orgs/$org/repos?access_token=$token" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"auto_init\": true, \"default_branch\": \"master\", \"description\": \"$repo\", \"license\": \"CC-BY-SA-4.0.md\", \"name\": \"$repo\", \"private\": false, \"readme\": \"Default\"}")
-echo "$response"
-
-echo "Press ENTER to continue"
-read
-
 #### CREATE FILE 1 IN master
 echo "
 =========
@@ -98,9 +97,6 @@ response=$(curl --silent -X POST "$host/api/v1/repos/$org/$repo/contents/$file1_
 echo "$response"
 file1_sha=$(echo "$response" | jq -r '.content.sha')
 echo -e "\nFILE 1 SHA: $file1_sha\n\n"
-
-echo "Press ENTER to continue"
-read
 
 #### CREATE FILE 2 IN master
 echo "
@@ -113,15 +109,21 @@ echo "$response"
 file2_sha=$(echo "$response" | jq -r '.content.sha')
 echo -e "\nFILE 2 SHA: $file2_sha\n\n"
 
+read -p "
+Press ENTER to continue"
+
 #### CREATE USER BRANCH
 #### Done by updating FILE 1 with same content but new branch
 echo "
 =========
-CREATING USER BRANCH:
+CREATING USER BRANCH $branch:
 
 "
 response=$(curl --silent -X PUT "$host/api/v1/repos/$org/$repo/contents/file1.md?access_token=$token" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"branch\": \"master\", \"content\": \"$file1_base64\", \"message\": \"Creates new branch\", \"new_branch\": \"$branch\", \"sha\": \"$file1_sha\"}")
 echo "$response"
+
+read -p "
+Press ENTER to continue"
 
 #### MODIFY FILE 1 IN USER BRANCH
 echo "
@@ -129,7 +131,8 @@ echo "
 MODIFYING $file1_name IN USER BRANCH:
 
 "
-response=$(curl --silent -X PUT "$host/api/v1/repos/$org/$repo/contents/$file1_name?access_token=$token" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"branch\": \"$branch\", \"content\": \"$user_modified_file1_base64\", \"message\": \"Updates $file1_name in user branch\", \"sha\": \"$file1_sha\"}")
+response=$(curl --silent -X PUT "$host/api/v1/repos/$org/$repo/contents/$file1_name?access_token=$token" -H "accept: application/json" -H "Content-Type: application/json" -d "{ 
+  \"branch\": \"$branch\", \"content\": \"$user_modified_file1_base64\", \"message\": \"Updates $file1_name in user branch\", \"sha\": \"$file1_sha\"}")
 echo "$response"
 
 #### MODIFY FILE 2 IN USER BRANCH
@@ -140,6 +143,9 @@ MODIFYING $file2_name IN USER BRANCH:
 "
 response=$(curl --silent -X PUT "$host/api/v1/repos/$org/$repo/contents/$file2_name?access_token=$token" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"branch\": \"$branch\", \"content\": \"$user_modified_file2_base64\", \"message\": \"Updates $file2_name in user branch\", \"sha\": \"$file2_sha\"}")
 echo "$response"
+
+read -p "
+Press ENTER to continue"
 
 #### MODIFY FILE 1 IN master BRANCH
 echo "
@@ -159,10 +165,13 @@ MODIFYING $file2_name IN master BRANCH:
 response=$(curl --silent -X PUT "$host/api/v1/repos/$org/$repo/contents/$file2_name?access_token=$token" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"branch\": \"master\", \"content\": \"$master_modified_file2_base64\", \"message\": \"Updates $file2_name in master branch\", \"sha\": \"$file2_sha\"}")
 echo "$response"
 
+read -p "
+Press ENTER to continue"
+
 #### MAKE PR FOR user branch into master
 echo "
 =========
-MAKING PR FOR USER BRANCH INTO master:
+MAKING PR FOR USER BRANCH $branch INTO master:
 
 "
 response=$(curl --silent -X POST "$host/api/v1/repos/$org/$repo/pulls?access_token=$token" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"base\": \"master\", \"body\": \"Merging user branch into master\", \"head\": \"$branch\", \"title\": \"$branch into master\"}")
@@ -174,7 +183,14 @@ diff_url=$(echo "$response" | jq -r '.diff_url')
 patch_url=$(echo "$response" | jq -r '.path_url')
 mergeable=$(echo "$response" | jq -r '.mergeable')
 
-echo -e "\n\nPR URL: $pr_url\nDIFF URL: $diff_url\nPATCH URL: $patch_url\nMERGEABLE: $mergeable\n"
+echo -e "
+PR URL: $pr_url
+DIFF URL: $diff_url
+
+MERGEABLE: $mergeable"
+
+read -p "
+Press ENTER to continue"
 
 if [[ $mergeable == "false" ]]; then
   echo -e "\nIS NOT MERGEABLE!"
@@ -185,7 +201,7 @@ echo -e "\nIS MERGEABLE!"
 #### MERGE PR
 echo "
 =========
-MERGING PR:
+MERGING PR FOR $branch INTO master:
 
 "
 response=$(curl --silent --write-out '%{http_code}' --output /dev/null -X POST "$host/api/v1/repos/$org/$repo/pulls/$pr_num/merge?access_token=$token" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"Do\": \"merge\", \"force_merge\": false}")
@@ -194,6 +210,9 @@ if [[ "$response" != "200" ]]; then
   exit
 fi
 echo -e "MERGE WAS SUCCESSFUL!\n\n"
+
+read -p "
+Press ENTER to continue"
 
 #### GET MERGED MASTER FILE 1
 echo "
@@ -219,6 +238,9 @@ else
   diff <(echo "$expected_file1_content") <(echo "$merged_file1_content")
 fi
 
+read -p "
+Press ENTER to continue"
+
 #### GET MERGED MASTER FILE 2
 echo "
 =========
@@ -242,6 +264,9 @@ else
   echo -e "DIFFERENT:\n\n"
   diff <(echo "$expected_file2_content") <(echo "$merged_file2_content")
 fi
+
+read -p "
+Press ENTER to continue"
 
 echo "
 ==========
